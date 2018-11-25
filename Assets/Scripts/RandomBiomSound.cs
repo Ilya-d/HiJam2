97fce6ef01e9494545425d84d2832be9b0269022
Quantum Felix <felixmrcat@gmail.com>
2018-11-25 20:53:36 +0200

    Update RandomBiomSound.cs

--- Assets/Scripts/RandomBiomSound.cs
+++ Assets/Scripts/RandomBiomSound.cs
@@ -18,7 +18,7 @@ public class RandomBiomSound : MonoBehaviour {
 
     void CallAudio()
     {
-        Invoke("RandomSoundness", 10);
+        Invoke("RandomSoundness", 25);
     }
 
     void RandomSoundness()
